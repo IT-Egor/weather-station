b1110e9d5866a79a5a105caf5bba78eb6e7fce61
IT-Egor <egor.belousov.04@yandex.ru>
2024-10-18 23:02:00 +0300

    Sensor test with library

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,50 @@
 #include <Arduino.h>
 
-void setup() {
-	Serial.begin(115200);
-	Serial.println("Blinking...");
-	pinMode(LED_BUILTIN, OUTPUT);
-}
+#include <Wire.h> 
+#include <iarduino_AM2320.h> 
+iarduino_AM2320 sensor;
 
+void setup() {                                                                    
+	sensor.begin(&Wire);
+	Serial.begin(115200);
+}                                                                                
+                                                                                 
 void loop() {
-	digitalWrite(LED_BUILTIN, HIGH);
-	delay(1000);
-	digitalWrite(LED_BUILTIN, LOW);
-	delay(1000);
-}
+	switch(sensor.read()) { 
+		case AM2320_OK:
+			Serial.print("Sensor AM2320: ");
+			Serial.print((String) "t="+sensor.tem+"°C, ");
+			Serial.print((String)"h="+sensor.hum+"%");
+			Serial.println();
+			break; 
+
+		case AM2320_ERROR_LEN:
+			Serial.println("Buffer size exceeded");
+			break; 
+
+		case AM2320_ERROR_ADDR: 
+			Serial.println("The sensor is not detected");
+			break; 
+
+		case AM2320_ERROR_DATA: 
+			Serial.println("Data error");
+			break; 
+
+		case AM2320_ERROR_SEND: 
+			Serial.println("Transmission error");
+		    break; 
+
+		case AM2320_ERROR_READ: 
+			Serial.println("Reading error");
+		    break; 
+
+		case AM2320_ERROR_ANS:  
+			Serial.println("The answer is incorrect");
+		    break; 
+
+		case AM2320_ERROR_LINE: 
+			Serial.println("Line interference"); 
+			break; 
+	}                                                                           
+	delay(1000);                                                                
+}                                                                                
