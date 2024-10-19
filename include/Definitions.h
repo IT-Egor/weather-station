9aca810a849c7c73da32c453e3041d308ae2a781
IT-Egor <egor.belousov.04@yandex.ru>
2024-10-19 21:09:12 +0300

    Add blinking while sensor is not ok

---
+++ include/Definitions.h
@@ -0,0 +1,5 @@
+#include <Arduino.h>
+
+uint64_t blinkTimer = 0;
+uint64_t scanTimer = 0;
+void blinking(bool on);
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <Arduino.h>
-
+#include <Definitions.h>
 #include <AM2320.h>
 #include <Wire.h>
 
@@ -14,28 +14,45 @@ void setup() {
 		Serial.println("AM2320 initialization failed");
 	}
 	Serial.println();
+	pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-	Conditions cond = sensor.getConditions();
-	Status sensorStatus = sensor.getStatus();
-	switch (sensorStatus) {
-		case Status::OK:
-			Serial.print((String) "t="+cond.temperature+"°C, ");
-			Serial.println((String) "h="+cond.humidity+"%");
-			break;
-		
-		case Status::READ_ERROR:
-			Serial.println("AM2320 read error");
-			break;
+	Status sensorStatus;
+	if (millis() - scanTimer > 1000) {
+		scanTimer = millis();
+		Conditions cond = sensor.getConditions();
+		sensorStatus = sensor.getStatus();
+		switch (sensorStatus) {
+			case Status::OK:
+				Serial.print((String) "t="+cond.temperature+"°C, ");
+				Serial.println((String) "h="+cond.humidity+"%");
+				break;
+			
+			case Status::READ_ERROR:
+				Serial.println("AM2320 read error");
+				break;
 
-		case Status::SENSOR_NOT_READY:
-			Serial.println("AM2320 sensor not ready");
-			break;
+			case Status::SENSOR_NOT_READY:
+				Serial.println("AM2320 sensor not ready");
+				break;
 
-		case Status::CRC_MISMATCH:
-			Serial.println("AM2320 CRC mismatch");
-			break;
-	};
-	delay(1000);
+			case Status::CRC_MISMATCH:
+				Serial.println("AM2320 CRC mismatch");
+				break;
+		};
+	}
+	if (sensorStatus != Status::OK) {
+		blinking(true);
+	} else {
+		blinking(false);
+	}
+}
+
+
+void blinking(bool on) {
+	if (on && millis() - blinkTimer > 100) {
+		blinkTimer = millis();
+		digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+	}
 }
