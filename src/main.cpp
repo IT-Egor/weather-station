c162d57a7108f8ef0c092fe2e6da27b04f9b0819
IT-Egor <egor.belousov.04@yandex.ru>
2024-10-19 20:10:25 +0300

    Code refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -19,25 +19,23 @@ void setup() {
 void loop() {
 	Conditions cond = sensor.getConditions();
 	Status sensorStatus = sensor.getStatus();
-	switch (sensorStatus)
-	{
-	case Status::OK:
-		Serial.print((String) "t="+cond.temperature+"°C, ");
-		Serial.println((String) "h="+cond.humidity+"%");
-		break;
-	
-	case Status::READ_ERROR:
-		Serial.println("AM2320 read error");
-		break;
-
-	case Status::SENSOR_NOT_READY:
-		Serial.println("AM2320 sensor not ready");
-		break;
-
-	case Status::CRC_MISMATCH:
-		Serial.println("AM2320 CRC mismatch");
-		break;
+	switch (sensorStatus) {
+		case Status::OK:
+			Serial.print((String) "t="+cond.temperature+"°C, ");
+			Serial.println((String) "h="+cond.humidity+"%");
+			break;
 		
+		case Status::READ_ERROR:
+			Serial.println("AM2320 read error");
+			break;
+
+		case Status::SENSOR_NOT_READY:
+			Serial.println("AM2320 sensor not ready");
+			break;
+
+		case Status::CRC_MISMATCH:
+			Serial.println("AM2320 CRC mismatch");
+			break;
 	};
 	delay(1000);
 }
